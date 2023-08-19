19b8b23e418ff62f2a3a196a258e7f7d220b20d5
ShashikantKokitakar <Shashikant.kokitakar@facillima.com>
2023-08-19 09:45:03 +0530

    new commit

---
+++ src/test/java/test1/NewTest.java
@@ -0,0 +1,48 @@
+package test1;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+
+public class NewTest {
+  @Test
+  public void f() 
+  {
+	  DesiredCapabilities caps= new DesiredCapabilities();
+	  
+	  caps.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+	  
+	  caps.setCapability(MobileCapabilityType.DEVICE_NAME, "MZIBVKPRAMCMK7GM");
+	  
+	  caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
+	  
+	  caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+	  
+	  caps.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+	  
+	  
+	  
+	  //Establishing the connection with appium server
+	  
+	  URL url = null;
+	try 
+	{
+		url = new URL("http://127.0.0.1:4723/wd/hub/");
+	}
+	catch (MalformedURLException e) 
+	{
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  
+      AndroidDriver  driver= new AndroidDriver(url,caps);
+      
+      driver.get("https://www.villeos.com/");
+  }
+}
