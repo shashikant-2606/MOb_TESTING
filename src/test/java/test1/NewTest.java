80f1bdbc9687231672228b4adc5309233f595de9
ShashikantKokitakar <Shashikant.kokitakar@facillima.com>
2023-08-21 19:39:15 +0530

    123

--- src/test/java/test1/NewTest.java
+++ src/test/java/test1/NewTest.java
@@ -10,16 +10,16 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 
-import com.microsoft.appcenter.appium.Factory;
-import com.microsoft.appcenter.appium.EnhancedAndroidDriver;
-import org.junit.rules.TestWatcher;
-import org.junit.Rule;
 
-public class NewTest {
+public class NewTest 
+{
+	protected static AndroidDriver driver;
   @Test
   public void f() 
   {
 	  DesiredCapabilities caps= new DesiredCapabilities();
+	  
+	  
 	  
 	  caps.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
 	  
@@ -30,9 +30,6 @@ public class NewTest {
 	  caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 	  
 	  caps.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
-	  
-	  TestWatcher watcher = Factory.createWatcher();
-	  
 	  
 	  
 	  //Establishing the connection with appium server
@@ -47,8 +44,7 @@ public class NewTest {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	EnhancedAndroidDriver driver;
-	  driver = Factory.createAndroidDriver(url, caps);
+	driver=new AndroidDriver(url, caps);
       
       driver.get("https://www.villeos.com/");
   }
