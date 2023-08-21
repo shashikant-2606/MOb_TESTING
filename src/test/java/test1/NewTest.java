e24e43be4d7a628d5763c42d4c998a0f035d1387
ShashikantKokitakar <Shashikant.kokitakar@facillima.com>
2023-08-21 18:58:30 +0530

    1

--- src/test/java/test1/NewTest.java
+++ src/test/java/test1/NewTest.java
@@ -40,7 +40,7 @@ public class NewTest {
 	  URL url = null;
 	try 
 	{
-		url = new URL("http://192.168.1.50:4723/wd/hub");
+		url = new URL("http://127.0.0.1:4723/wd/hub/");
 	}
 	catch (MalformedURLException e) 
 	{
